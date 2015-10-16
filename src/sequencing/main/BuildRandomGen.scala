9cb9e04a5d03b9dfd047532c55d7e365a3c31f36
danglobt <bdanglot@gmail.com>
2015-10-16 12:42:51 +0200

    fix BuildRandomGen.scala

--- src/sequencing/main/BuildRandomGen.scala
+++ src/sequencing/main/BuildRandomGen.scala
@@ -4,16 +4,7 @@ package sequencing.main
  * @author danglot
  */
 object BuildRandomGen extends App {
-  
-  def IntToC(i : Int) : String = {
-    i match {
-      case 0 => "A"
-      case 1 => "C"
-      case 2 => "G"
-      case 3 => "T"
-    }
-  }
-  
+
   val pathname = "input/read"
 
   val number = 1000
@@ -25,7 +16,7 @@ object BuildRandomGen extends App {
   val r = new java.util.Random()
   
   while (i != number) {
-   writer.write(IntToC(r.nextInt(4)))
+   writer.write(sequencing.util.Parser.IntToC(r.nextInt(4)))
    i += 1
    if (i % 1000 == 0)
      writer.write("\n")
